5fd144d0e4d0ed158d029d1766d0494cc4e323b1
Gautam Panakkal <gautampanakkal@gmail.com>
2024-11-05 13:14:46 -0800

    Updating routes
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

---
+++ src/lib/hash.ts
@@ -0,0 +1,91 @@
+// const crypto = require('crypto')
+import { ClientSessionAttribute } from '@fflags/types';
+import crypto from 'node:crypto';
+/*
+MD5 vs DJB2:
+- MD5 is more collision resistant so it is prefereable to be used for larget data sets.
+- DJB2 is faster and more lightweight, as it relies on bitwise operations and some arithmetic. Ideal for frequent hashing. MD5 is has greater computation costs
+- MD5 produces a larger range of values (128bit hash) (less collision resistance) than DJB2 (32bit hash)
+
+- MD5 is better suited where more collision resistance is needed, DJB2 is ideal for fast hash computations
+- MD5 adds more dependency by use of crypto module, DJB2 has no depdencies.
+
+*/
+
+// function hashIdentifiers <T extends boolean | string | number> (identifiers: { [attributeName: string]: string }, flagValues: Set<T>): T {
+//
+
+type Identifier = ClientSessionAttribute;
+
+// }
+//DJB2 Hash function.
+function hashIdentifiers(identifiers: Identifier[]) {
+  const sortedIdentifiers = identifiers.sort((a, b) => (a.name > b.name) ? 1 : -1);
+  let string = '';
+
+  sortedIdentifiers.forEach(({ name, dataType, value }) => {
+    string += name + value;
+  });
+  
+  let hash = 0;
+  //iterate over the string, use bitwise left shift operator, which is essentially multiply the value by 32 -- increases significance of the current hash value.
+  // subtract the hash from the result
+  //Add the utf char value
+  for (let i = 0; i < string.length; i++) {
+    hash = (hash << 5) - hash + string.charCodeAt(i); 
+    hash |= 0; // Convert to 32bit integer -- bitwise OR operator(?)
+  }
+  // console.log({stringIdentifiers});
+  // return stringIdentifiers;
+  return hash;
+}
+
+/**
+ * 
+ * @param identifiers 
+ * @param assignmentOptions A set of ExperimentBlock ids
+ * @returns 
+ */
+export function hashAndAssign<BlockId extends string>(identifiers: Identifier[], assignmentOptions: BlockId[]): BlockId {
+  const hash = hashIdentifiers(identifiers);
+  const flagArray = [...assignmentOptions].sort();
+  // console.log(flagArray);
+
+  const index = Math.abs(hash) % flagArray.length;
+  return flagArray[index];
+}
+
+const flagValues = ['active', 'inactive', 'pending', 'suspended'];
+
+// console.log(hashAndAssign({z: 'z',  az: 'a', ab: 'ab', bc: 'b', email: 'sean.a.mentele@gmail.com', name: 'sean'}, flagValues) === hashAndAssign({ email: 'sean.a.mentele@gmail.com', name: 'sean', z: 'z',  az: 'a', ab: 'ab', bc: 'b'}, flagValues));
+// console.log(hashAndAssign({name: 'sean', email: 'sean.a.mentele@gmail.com'}, flagValues) ===  hashAndAssign({email: 'sean.a.mentele@gmail.com', name: 'sean'}, flagValues))
+// console.log(hashAndAssign({userId: "12345", userName: "john_doe"}, flagValues));
+// console.log(hashAndAssign({name: 'sean', email: 'sean.a.mentele@gmail.com'}, flagValues));
+
+function hashIdentifiersMD5(identifiers: Identifier[]) {
+  const sortedKeys = Object.keys(identifiers).sort();
+  let string = '';
+
+  sortedKeys.forEach((key) => {
+    string += identifiers[key];
+  });
+  
+  //Create a new MD5 hash instance -> Produces 128bit hash value (32char hexadecimal string)
+  const hexString = crypto.createHash('md5')
+    .update(string) //Feed the input string into the hash instance
+    .digest('hex'); //Specify hex to return a hexidecimal string
+  const hash = BigInt('0x' + hexString); //Converts to BigInt
+
+  return hash;
+}
+
+function hashAndAssignMD5(identifiers: Identifier[], flagValues=['Control', 'Variant']) {
+  const hashInt = hashIdentifiersMD5(identifiers);
+  const flagArray = Array.from(flagValues).sort();
+
+  const index = Number(hashInt % BigInt(flagArray.length))
+
+  return flagArray[index];
+}
+
+console.log(hashAndAssignMD5({z: 'z',  az: 'a', ab: 'ab', bc: 'b', email: 'sean.a.mentele@gmail.com', name: 'sean'}, flagValues));
