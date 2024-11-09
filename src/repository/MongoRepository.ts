0dd653917283a4ba59e0bc59277c18329acbc97b
Gautam Panakkal <gautampanakkal@gmail.com>
2024-11-08 17:38:43 -0800

    MongoRepository finished with single-doc CRUD as well as getMany and find methods; all tests passing
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- src/repository/MongoRepository.ts
+++ src/repository/MongoRepository.ts
@@ -1,5 +1,4 @@
-import { MongoClient, ObjectId, Document, Collection, Db, Filter } from 'mongodb';
-import env from '../envalid.js';
+import { MongoClient, ObjectId, Document, Collection, Db, Filter, OptionalUnlessRequiredId } from 'mongodb';
 import { EstuarySchema } from '@estuary/types';
 import { EstuaryBaseTypes, MongoRecord, BeforeId, PartialUpdate } from './MongoRepository.types.js';
 
@@ -33,11 +32,11 @@ export default class MongoRepository<T extends EstuaryBaseTypes> {
   #client: MongoClient;
   #db: Db;
   schema: EstuarySchema<T>;
-  collection: Collection<MongoRecord<T>>;
+  collection: Collection<BeforeId<T>>;
   // environments: Collection<Environment>;
 
-  constructor(collectionName: string, schema: EstuarySchema<T>, mongoUri?: string) {
-    this.#client = new MongoClient(mongoUri ?? env.MONGO_URI);
+  constructor(collectionName: string, schema: EstuarySchema<T>, mongoUri: string) {
+    this.#client = new MongoClient(mongoUri);
     this.#db = this.#client.db();
     this.schema = schema;
     this.collection = this.#db.collection(collectionName);
@@ -72,7 +71,6 @@ export default class MongoRepository<T extends EstuaryBaseTypes> {
     console.log(`${count} records in '${this.collection.collectionName}' collection`);
     return count;
   }
-
   /**
    * Get up to `maxCount` documents, or all if not specified
    * @returns a possibly empty array of documents
@@ -85,71 +83,53 @@ export default class MongoRepository<T extends EstuaryBaseTypes> {
     const transformed = documents.map((doc) => this._recordToObject(doc));
     return transformed;
   }
-
   /**
    * @param documentId a hex string representing an ObjectId
    */
   async get(documentId: string): Promise<T | null> {
     const docId = ObjectId.createFromHexString(documentId);
-    const query: Filter<MongoRecord<T>> = { _id: docId } as Filter<MongoRecord<T>>; // placeholder - seems like Filter doesn't infer correctly on generics
-    const result = await this.find(query);
-    if (result === null) return result;
-    return this._recordToObject(result);
+    // seems like Filter doesn't infer correctly on generics
+    const result = await this.find({ _id: docId } as Filter<BeforeId<T>>);
+    return result;
   }
-
   /**
-   * Find a flag document from any of its properties. An empty object matches all documents.
+   * Find a document from any of its properties. An empty object matches all documents.
    * To find by name, pass { name: <documentName> }
    * See https://www.mongodb.com/docs/drivers/node/current/fundamentals/crud/query-document/#std-label-node-fundamentals-query-document
    * @param query A MongoDB query
    */
-  async find(query: Filter<MongoRecord<T>>): Promise<T | null> {
+  async find(query: Filter<BeforeId<T>>): Promise<T | null> {
     const result = await this.collection.findOne(query);
     if (result === null) return result;
     return this._recordToObject(result);
   }
-
-  async findMatchingFlags(query: Filter<MongoRecord<T>>): Promise<T[]> {
-    const result = await this.collection.find(query);
-    const docs = await result.toArray();
-    return docs.map(this._recordToObject);
-  }
   /**
    * @returns a hex string representing the new record's ObjectId
    */
-  async createFlag(flag: BeforeId<T>): Promise<string | null> {
-    if ('id' in flag) { // placeholder
-      throw new Error(`Argument has id ${flag.id}, suggesting the record already exists`);
-    }
+  async create(newEntry: OptionalUnlessRequiredId<BeforeId<T>>): Promise<string | null> {
 
-    const result = await this.collection.insertOne(flag);
+    const result = await this.collection.insertOne({ ...newEntry });
     return result.insertedId?.toHexString() ?? null;
   }
 
   /**
    * Updates an existing record
-   * @returns a hex string representing the updated record's ObjectId,
-   * or null if no record was updated
+   * @returns true if a record was updated, or false otherwise
    */
-  async update(flag: PartialUpdate<T>): Promise<string | null> {
-    const { id, ...updates } = flag;
-    const result = await this.collection.updateOne({ _id: ObjectId.createFromHexString(id)}, updates);
-    return result.upsertedId?.toHexString() ?? null;
+  async update(partialEntry: PartialUpdate<T>) {
+    const { id, ...updates } = partialEntry;
+    const filter = { _id: ObjectId.createFromHexString(id) } as Filter<BeforeId<T>>;
+    const result = await this.collection.updateOne(filter, [{ $set: updates }]);
+    return result.modifiedCount > 0;
   }
 
   /**
-   * Deletes an existing flag
+   * Deletes an existing record
    * @returns true if a record was deleted, or false otherwise
    */
-  async deleteFlag(documentId: string): Promise<boolean> {
+  async delete(documentId: string): Promise<boolean> {
     const filter = { _id: ObjectId.createFromHexString(documentId)};
-    const result = await this.collection.deleteOne(filter);
+    const result = await this.collection.deleteOne(filter as Filter<BeforeId<T>>);
     return result.deletedCount === 1;
   }
 }
-
-// example usage
-// const db = new MongoAPI(env.MONGO_TESTING_URI);
-// const xformResult = db._flagRecordToObject({ _id: ObjectId.createFromHexString('672554f934265b61cb05d5cf'), name: 'example record' });
-// console.log({xformResult})
-
