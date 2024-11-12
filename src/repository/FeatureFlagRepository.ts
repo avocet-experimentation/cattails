82db87fdbda734a56cf5240a89db814daf8b9f5e
Gautam Panakkal <gautampanakkal@gmail.com>
2024-11-11 16:12:04 -0800

    Fixed broken imports of MongoRepository subclasses
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- src/repository/FeatureFlagRepository.ts
+++ src/repository/FeatureFlagRepository.ts
@@ -3,12 +3,12 @@ import MongoRepository from "./MongoRepository.js";
 import { BeforeId, PartialUpdate } from "./MongoRepository.types.js";
 import { Filter, ObjectId } from "mongodb";
 
-export class FeatureFlagRepository extends MongoRepository<FeatureFlag> {
+export default class FeatureFlagRepository extends MongoRepository<FeatureFlag> {
   constructor(mongoUri: string) {
     super('flags', featureFlagSchema, mongoUri);
   }
   /**
-   * Add a rule 
+   * Add an override rule 
    */
     async addRule(flagId: string, environmentName: string, rule: OverrideRule) {
       const result = await this.push({ 
