bbbc04811f3d5bcff47643984691fcae9d635d89
Gautam Panakkal <gautampanakkal@gmail.com>
2024-11-11 20:04:16 -0800

    Client routes and repository changes: - Client routes send a hash value along with each feature flag value - new method on MongoRepository for finding multiple matching documents - new method on FeatureFlagRepository for fetching all flags enabled in the passed environment
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- src/routes/client.controller.ts
+++ src/routes/client.controller.ts
@@ -2,20 +2,24 @@ import {
   ClientPropMapping,
   flagClientValueSchema,
   FlagClientValue,
-  FlagClientMapping
+  FlagClientMapping,
 } from "@estuary/types";
 import { FastifyReply, FastifyRequest } from "fastify";
-import MongoAPI from "../lib/MongoAPI.js";
-import env from "../envalid.js";
 import { currentFlagValue } from "../lib/flagValue.js";
+import { getClientRepos } from "../repository/index.js";
+import { printDetail } from "../lib/index.js";
 
-const mongoApi = new MongoAPI(env.MONGO_API_URI);
+const { fflagRepo } = getClientRepos();
 // until we have API keys corresponding to environments
 const PLACEHOLDER_ENVIRONMENT = 'dev';
 
-type FetchFlagRequestParams = { 
-  Params: { fflagName: string, }, 
+interface FetchFlagsBody { 
   Body: { environment: string, clientSessionAttributes: ClientPropMapping},
+}
+
+interface FetchFlagRequest extends FetchFlagsBody { 
+  Params: { fflagName: string, }, 
+  // Body: { environment: string, clientSessionAttributes: ClientPropMapping},
 }
 
 /**
@@ -24,40 +28,41 @@ type FetchFlagRequestParams = {
  * - use key to identify environment
  */
 export const fetchFFlagHandler = async (
-  request: FastifyRequest<FetchFlagRequestParams>,
+  request: FastifyRequest<FetchFlagRequest>,
   reply: FastifyReply
 ): Promise<FlagClientValue> => {
   const { fflagName } = request.params;
-  const { clientSessionAttributes } = request.body;
-  const fflag = await mongoApi.findFlag({ name: fflagName });
-  if (!fflag) {
+  const { environment, clientSessionAttributes } = request.body;
+  const featureFlag = await fflagRepo.findOne({ name: fflagName });
+  if (!featureFlag) {
     return reply
       .code(404)
       .send({ error: { code: 404, message: "flag not found" } });
   }
 
-  const currentValue = currentFlagValue(fflag, PLACEHOLDER_ENVIRONMENT, clientSessionAttributes);
+  const currentValue = currentFlagValue(featureFlag, PLACEHOLDER_ENVIRONMENT, clientSessionAttributes);
 
   return flagClientValueSchema.parse(currentValue);
 };
 
 export const getEnvironmentFFlagsHandler = async (
-  request: FastifyRequest<{ Params: { environmentName: string } }>,
+  request: FastifyRequest<FetchFlagsBody>,
   reply: FastifyReply
 ): Promise<FlagClientMapping> => {
-  const { environmentName } = request.params;
-  const fflags = await mongoApi.findMatchingFlags({
-    environmentName
-  });
-  if (!fflags) {
+  const { environment, clientSessionAttributes } = request.body;
+  const featureFlags = await fflagRepo.getEnvironmentFlags(environment);
+  if (!featureFlags) {
     return reply
       .code(404)
       .send({ error: { code: 404, message: "flags not found" } });
   }
 
-  const mapping = fflags.reduce((acc, el) => {
-    Object.assign(acc, { [el.name]: el });
+  // printDetail({ featureFlags });
+  // console.log({ overrideRules: fflags[0].environments.dev?.overrideRules });
+
+  const mapping = featureFlags.reduce((acc, featureFlag) => {
+    Object.assign(acc, { [featureFlag.name]: currentFlagValue(featureFlag, PLACEHOLDER_ENVIRONMENT, clientSessionAttributes) });
     return acc;
-  }, {});
+  }, {} as FlagClientMapping);
   return mapping;
 };
