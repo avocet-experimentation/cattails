31448385834c3e0a0b1cfe966054433978246272
Gautam Panakkal <gautampanakkal@gmail.com>
2024-11-13 22:24:32 -0800

    Small bugfixes: Fixed context loss when transforming arrays of fetched records to objects; temporarily commented out buggy push methods on Repository classes; added a missing property on test data
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- src/repository/FeatureFlagRepository.ts
+++ src/repository/FeatureFlagRepository.ts
@@ -5,16 +5,16 @@ export default class FeatureFlagRepository extends MongoRepository<FeatureFlag, 
   constructor(mongoUri: string) {
     super('FeatureFlag', featureFlagSchema, mongoUri);
   }
-  /**
-   * Add an override rule 
-   */
-  async addRule(flagId: string, environment: string, rule: OverrideRule) {
-    const result = await this.push({ 
-      id: flagId, 
-      [`environments.${environment}.overrideRules`]: rule,
-    });
-    return result;
-  }
+  // /**
+  //  * Add an override rule 
+  //  */
+  // async addRule(flagId: string, environment: string, rule: OverrideRule) {
+  //   const result = await this.push({ 
+  //     id: flagId, 
+  //     [`environments.${environment}.overrideRules`]: rule,
+  //   });
+  //   return result;
+  // }
 
   async getEnvironmentFlags(environment: string) {
     return this.findMany({ [`environments.${environment}.enabled`]: true });
