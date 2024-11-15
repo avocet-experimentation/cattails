aea03ac7fa6f868037018c81ba62b8029b0e399f
Gautam Panakkal <gautampanakkal@gmail.com>
2024-11-15 11:25:04 -0800

    Added some unique and wildcard indexes on FeatureFlag and Experiment collections
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- src/repository/index.ts
+++ src/repository/index.ts
@@ -5,10 +5,16 @@ import ExperimentRepository from "./ExperimentRepository.js";
  * Use getAdminRepos or getClientRepos
  */
 export function getAdminRepos() {
-  return {
-    fflagRepo: new FeatureFlagRepository(env.MONGO_ADMIN_URI),
-    experimentRepo: new ExperimentRepository(env.MONGO_ADMIN_URI),
-  }
+  const repos = {
+    featureFlag: new FeatureFlagRepository(env.MONGO_ADMIN_URI),
+    experiment: new ExperimentRepository(env.MONGO_ADMIN_URI),
+  };
+
+  repos.featureFlag.collection.createIndex({ 'name': 1 }, { unique: true, });
+  repos.featureFlag.collection.createIndex({ 'environments.$**': 1 });
+  repos.experiment.collection.createIndex({ 'name': 1 }, { unique: true, });
+
+  return repos;
 }
 
 export function getClientRepos() {
