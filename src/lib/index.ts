874a2fdcef7df31980e358b685dfd737c4af14f6
Gautam Panakkal <gautampanakkal@gmail.com>
2024-11-21 15:29:23 -0800

    Small fix for `printDetail` helper
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Luke Carlson <60205986+lapis-lark@users.noreply.github.com>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,11 +1,11 @@
 import util from 'node:util';
 
 export const printDetail = (obj: unknown) => {
-  console.log(util.inspect(obj), {
+  console.log(util.inspect(obj, {
     depth: null,
     colors: true,
     maxArrayLength: null,
     maxStringLength: null,
     compact: false,
-  });
+  }));
 };
