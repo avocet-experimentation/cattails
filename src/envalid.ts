d96348b2bf0490de0c9ee7a38fa4b967aeb6f380
Adnan Shihabi <adnan@ashihabi.me>
2024-12-21 15:54:21 -0800

    update: additional properties for `cfg` cleanEnv object
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>
    Co-authored-by: Gautam Panakkal <111669555+gpanakkal@users.noreply.github.com>

--- src/envalid.ts
+++ src/envalid.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { cleanEnv, num, str } from "envalid";
+import { cleanEnv, num, str } from 'envalid';
 
 const cfg = cleanEnv(process.env, {
   MONGO_DATABASE: str(),
@@ -9,6 +9,11 @@ const cfg = cleanEnv(process.env, {
   MONGO_TESTING_URI: str(),
   SERVICE_PORT: num(),
   SALT_ROUNDS: num(),
+  AUTH0_DOMAIN: str(),
+  AUTH0_AUDIENCE: str(),
+  AUTH0_CLIENT_ID: str(),
+  AUTH0_CLIENT_SECRET: str(),
+  DASHBOARD_URL: str(),
 });
 
 export default cfg;
