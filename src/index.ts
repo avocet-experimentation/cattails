6b45ae8fbc1bb4baa129d922688b9185ca0ab903
Adnan Shihabi <adnan@ashihabi.me>
2024-12-21 15:53:52 -0800

    add: Fastify implementation of JWT auth and `cors` config
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>
    Co-authored-by: Gautam Panakkal <111669555+gpanakkal@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@
 import { fastifyJwt } from '@fastify/jwt';
 import Fastify from 'fastify';
 import mercurius from 'mercurius';
-import mercuriusLogging from 'mercurius-logging';
-import cors from '@fastify/cors';
 import { schema } from './graphql/schemas.js';
 import { resolvers } from './graphql/resolvers.js';
 import cfg from './envalid.js';
@@ -12,15 +10,15 @@ import { jwtValidationObject } from './validation.js';
 
 const server = Fastify({
   logger: true,
-  disableRequestLogging: true,
 });
+
 // check if service is up during deployment; check on regular frequency
 server.get('/healthcheck', async () => ({ status: 'OK' }));
 // register routes for out flag entity
-server.register(getClientRoutes, { prefix: 'api' });
+await server.register(getClientRoutes, { prefix: 'api' });
 // todo: replace '*' origin with environment variable referencing dashboard
-server.register(cors, { prefix: 'graphql', origin: '*' });
-server.register(mercurius, {
+// await server.register(cors, { prefix: 'graphql', origin: '*' });
+await server.register(mercurius, {
   schema,
   resolvers,
   graphiql: true,
@@ -49,8 +47,6 @@ server.addHook('onRequest', async (request, reply) => {
     reply.code(401).send({ error: 'Unauthorized' });
   }
 });
-
-server.register(mercuriusLogging);
 
 server.listen({ port: cfg.SERVICE_PORT }, (error, address) => {
   if (error instanceof Error) {
