69024cbd9947048d4dc462183a13bcb59bbc3f8a
Gautam Panakkal <gautampanakkal@gmail.com>
2024-12-24 12:05:55 -0800

    Reorganize index per Fastify guide
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import { fastify, FastifyInstance } from 'fastify';
+import Fastify from 'fastify';
 import mercurius from 'mercurius';
 import cors from '@fastify/cors';
 import { schema } from './graphql/schemas.js';
@@ -8,35 +8,27 @@ import cfg from './envalid.js';
 import { getClientRoutes } from './routes/client.routes.js';
 import { getAdminRoutes } from './routes/admin.routes.js';
 
-export const buildServer = async (): Promise<FastifyInstance> => {
-  const server = fastify({
-    logger: true,
-  });
-  // check if service is up during deployment; check on regular frequency
-  server.get('/healthcheck', async () => ({ status: 'OK' }));
-  // register routes for out flag entity
-  await server.register(getClientRoutes, { prefix: 'api' });
-  await server.register(getAdminRoutes, { prefix: 'admin' });
-  // todo: replace '*' origin with environment variable referencing dashboard
-  await server.register(cors, { prefix: 'graphql', origin: '*' });
-  await server.register(mercurius, {
-    schema,
-    resolvers,
-    graphiql: true,
-  });
-  return server;
-};
+const server = Fastify({
+  logger: true,
+});
+// check if service is up during deployment; check on regular frequency
+server.get('/healthcheck', async () => ({ status: 'OK' }));
+// register routes for out flag entity
+server.register(getClientRoutes, { prefix: 'api' });
+server.register(getAdminRoutes, { prefix: 'admin' });
+// todo: replace '*' origin with environment variable referencing dashboard
+server.register(cors, { prefix: 'graphql', origin: '*' });
+server.register(mercurius, {
+  schema,
+  resolvers,
+  graphiql: true,
+});
 
-const main = async (): Promise<void> => {
-  const PORT = cfg.SERVICE_PORT;
-  try {
-    const server = await buildServer();
-    await server.listen({ port: PORT });
-    console.log(`cattails server ready at port ${PORT}`);
-  } catch (error) {
+server.listen({ port: cfg.SERVICE_PORT }, (error, address) => {
+  if (error instanceof Error) {
     console.error(error);
     process.exit(1);
   }
-};
 
-main();
+  console.log(`\ncattails server ready at ${address}`);
+});
