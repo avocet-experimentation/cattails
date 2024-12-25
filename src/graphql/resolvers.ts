345cba89e39c7d1e071bcdb1b286310107fd1976
Gautam Panakkal <gautampanakkal@gmail.com>
2024-12-24 18:05:22 -0800

    GraphQL: - Add interface for OverrideRule and implementing types for ExperimentReference and ForcedValue - Reorganize resolvers - (WIP) Add custom directive to narrow field from enum
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,37 +1,56 @@
+/* eslint-disable no-underscore-dangle */
 import {
-  ClientPropDef,
-  SDKConnection,
-  User,
   Environment,
-  Experiment,
-  ClientPropDefDraft,
-  ExperimentDraft,
-  SDKConnectionDraft,
-  UserDraft,
-  FeatureFlagDraft,
-  FeatureFlag,
   BeforeId,
   stripKeysWithUndefined,
-  EnvironmentDraft,
+  featureFlagSchema,
+  clientPropValueSchema,
+  OverrideRuleUnion,
 } from '@avocet/core';
 import { Filter } from 'mongodb';
 import { IResolvers } from 'mercurius';
+import { GraphQLScalarType } from 'graphql';
 import RepositoryManager from '../repository/RepositoryManager.js';
 import cfg from '../envalid.js';
-import { PartialWithStringId } from '../repository/repository-types.js';
-import {
-  environmentRecordScalar,
-  flagValueDefScalar,
-} from './featureFlagGQLSchema.js';
-import { clientPropValueScalar } from './clientPropDefGQLSchema.js';
+import { mutationResolvers } from './mutationResolvers.js';
 
 const repos = new RepositoryManager(cfg.MONGO_ADMIN_URI);
 
-export const resolvers: IResolvers = {
-  EnvironmentNames: environmentRecordScalar,
-  FlagValueDef: flagValueDefScalar,
-  ClientPropValue: clientPropValueScalar,
-  // #region Reader resolvers
+const scalarResolvers: IResolvers = {
+  OverrideRule: {
+    resolveType(obj: OverrideRuleUnion) {
+      const mapping = {
+        Experiment: 'ExperimentReference',
+        ForcedValue: 'ForcedValue',
+      };
+
+      const gqlType = mapping[obj.type];
+      console.log({ gqlType });
+      if (!gqlType) throw new TypeError(`type ${obj.type} not accounted for!`);
+      return gqlType;
+    },
+  },
+  EnvironmentNames: new GraphQLScalarType({
+    name: 'EnvironmentNames',
+    parseValue(value) {
+      return featureFlagSchema.shape.environmentNames.parse(value);
+    },
+  }),
+  FlagValueDef: new GraphQLScalarType({
+    name: 'FlagValueDef',
+    parseValue(value) {
+      return featureFlagSchema.shape.value.parse(value);
+    },
+  }),
+  ClientPropValue: new GraphQLScalarType({
+    name: 'ClientPropValue',
+    parseValue(value) {
+      return clientPropValueSchema.parse(value);
+    },
+  }),
+};
+
+const queryResolvers: IResolvers = {
   Query: {
     FeatureFlag: async (_, { id }: { id: string }) => repos.featureFlag.get(id),
     allFeatureFlags: async (_, { limit }: { limit?: number }) =>
@@ -66,181 +85,10 @@ export const resolvers: IResolvers = {
     allUsers: async (_, { limit }: { limit?: number }) =>
       repos.user.getMany(limit),
   },
-  // #endregion
-  Mutation: {
-    // #region FeatureFlag
-    createFeatureFlag: async (
-      _,
-      { newEntry }: { newEntry: FeatureFlagDraft },
-    ): Promise<FeatureFlag> => {
-      const flagId = await repos.featureFlag.create(newEntry);
-      return repos.featureFlag.get(flagId);
-    },
+};
 
-    updateFeatureFlag: async (
-      _,
-      { partialEntry }: { partialEntry: PartialWithStringId<FeatureFlag> },
-    ): Promise<FeatureFlag> => {
-      const success = await repos.featureFlag.update(partialEntry);
-
-      if (!success) {
-        throw new Error('Failed to update FeatureFlag');
-      }
-
-      return repos.featureFlag.get(partialEntry.id);
-    },
-
-    deleteFeatureFlag: async (_, { id }: { id: string }): Promise<string> => {
-      await repos.featureFlag.delete(id);
-      return id;
-    },
-    // #endregion
-
-    // #region Experiment
-    createExperiment: async (
-      _,
-      { newEntry }: { newEntry: ExperimentDraft },
-    ): Promise<Experiment> => {
-      const id = await repos.experiment.create(newEntry);
-      return repos.experiment.get(id);
-    },
-
-    updateExperiment: async (
-      _,
-      { partialEntry }: { partialEntry: PartialWithStringId<Experiment> },
-    ): Promise<Experiment> => {
-      const success = await repos.experiment.update(partialEntry);
-
-      if (!success) {
-        throw new Error('Failed to update the experiment');
-      }
-
-      return repos.experiment.get(partialEntry.id);
-    },
-
-    deleteExperiment: async (_, { id }: { id: string }): Promise<string> => {
-      await repos.experiment.delete(id);
-      return id;
-    },
-    // #endregion
-
-    // #region Environment
-    createEnvironment: async (
-      _,
-      { newEntry }: { newEntry: EnvironmentDraft },
-    ): Promise<Environment> => {
-      const id = await repos.environment.create(newEntry);
-      return repos.environment.get(id);
-    },
-
-    updateEnvironment: async (
-      _,
-      {
-        partialEntry,
-      }: {
-        partialEntry: PartialWithStringId<Environment>;
-      },
-    ): Promise<Environment> => {
-      const success = await repos.environment.update(partialEntry);
-      if (!success) {
-        throw new Error(`Failed to update environment ${partialEntry.id}`);
-      }
-
-      return repos.environment.get(partialEntry.id);
-    },
-
-    deleteEnvironment: async (_, { id }: { id: string }): Promise<string> => {
-      await repos.environment.delete(id);
-      return id;
-    },
-    // #endregion
-
-    // #region SDKConnection
-    createSDKConnection: async (
-      _,
-      { newEntry }: { newEntry: SDKConnectionDraft },
-    ): Promise<SDKConnection> => {
-      const id = await repos.sdkConnection.create(newEntry);
-      return repos.sdkConnection.get(id);
-    },
-
-    updateSDKConnection: async (
-      _,
-      {
-        partialEntry,
-      }: {
-        partialEntry: PartialWithStringId<SDKConnection>;
-      },
-    ): Promise<SDKConnection> => {
-      const success = await repos.sdkConnection.update(partialEntry);
-      if (!success) {
-        throw new Error(`Failed to update SDK connection ${partialEntry.id}`);
-      }
-
-      return repos.sdkConnection.get(partialEntry.id);
-    },
-
-    deleteSDKConnection: async (_, { id }: { id: string }): Promise<string> => {
-      await repos.sdkConnection.delete(id);
-      return id;
-    },
-    // #endregion
-
-    // #region ClientPropDef
-    createClientPropDef: async (
-      _,
-      { newEntry }: { newEntry: ClientPropDefDraft },
-    ): Promise<ClientPropDef> => {
-      const id = await repos.clientPropDef.create(newEntry);
-      return repos.clientPropDef.get(id);
-    },
-
-    updateClientPropDef: async (
-      _,
-      { partialEntry }: { partialEntry: PartialWithStringId<ClientPropDef> },
-    ): Promise<ClientPropDef> => {
-      const success = await repos.clientPropDef.update(partialEntry);
-      if (!success) {
-        throw new Error(
-          `Failed to update client property definition ${partialEntry.id}`,
-        );
-      }
-
-      return repos.clientPropDef.get(partialEntry.id);
-    },
-
-    deleteClientPropDef: async (_, { id }: { id: string }): Promise<string> => {
-      await repos.clientPropDef.delete(id);
-      return id;
-    },
-    // #endregion
-
-    // #region User
-    createUser: async (
-      _,
-      { newEntry }: { newEntry: UserDraft },
-    ): Promise<User> => {
-      const id = await repos.user.create(newEntry);
-      return repos.user.get(id);
-    },
-
-    updateUser: async (
-      _,
-      { partialEntry }: { partialEntry: PartialWithStringId<User> },
-    ): Promise<User> => {
-      const success = await repos.user.update(partialEntry);
-
-      if (!success) {
-        throw new Error(`Failed to update user ${partialEntry.id}`);
-      }
-
-      return repos.user.get(partialEntry.id);
-    },
-
-    deleteUser: async (_, { id }: { id: string }): Promise<string> => {
-      await repos.user.delete(id);
-      return id;
-    },
-    // #endregion
-  },
+export const resolvers: IResolvers = {
+  ...scalarResolvers,
+  ...queryResolvers,
+  ...mutationResolvers,
 };
