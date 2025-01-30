f1107ed2dce1f5fd7af5d591c4c4787e5617ce09
Gautam Panakkal <gautampanakkal@gmail.com>
2025-01-29 20:20:15 -0800

    Add JWT authentication check on mercurius routes; clean up unused packages
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import mercurius from 'mercurius';
 import mercuriusLogging from 'mercurius-logging';
 import cors from '@fastify/cors';
 import { fastifyAuth0Verify } from 'fastify-auth0-verify';
+
 import cfg from './envalid.js';
 import { schema } from './graphql/schemas.js';
 import { resolvers } from './graphql/resolvers.js';
@@ -15,15 +16,25 @@ const server = Fastify({
 });
 // check if service is up during deployment; check on regular frequency
 server.get('/healthcheck', async () => ({ status: 'OK' }));
-// register routes for out flag entity
 server.register(getClientRoutes, { prefix: 'api' });
-server.register(mercurius, {
-  schema,
-  resolvers,
-  graphiql: true,
+
+server.register(fastifyAuth0Verify, {
+  audience: cfg.AUTH0_AUDIENCE,
+  domain: cfg.AUTH0_DOMAIN,
+  secret: cfg.AUTH0_CLIENT_SECRET,
 });
 
-server.register(mercuriusLogging);
+server.register((instance, opts, next) => {
+  instance.addHook('onRequest', instance.authenticate);
+  instance.register(mercurius, {
+    schema,
+    resolvers,
+    graphiql: true,
+  });
+
+  server.register(mercuriusLogging);
+  next();
+});
 
 // configure cors
 await server.register(cors, {
@@ -33,23 +44,6 @@ await server.register(cors, {
   methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   maxAge: 86400,
-});
-
-await server.register(fastifyAuth0Verify, {
-  audience: cfg.AUTH0_AUDIENCE,
-  domain: cfg.AUTH0_DOMAIN,
-  secret: cfg.AUTH0_CLIENT_SECRET,
-});
-
-// middleware to authorize all requests
-server.addHook('onRequest', async (request, reply) => {
-  try {
-    console.log(request.headers.authorization);
-    await request.jwtVerify();
-  } catch (error) {
-    console.log(error);
-    reply.code(401).send({ error: 'Unauthorized' });
-  }
 });
 
 server.listen({ port: cfg.SERVICE_PORT }, (error, address) => {
