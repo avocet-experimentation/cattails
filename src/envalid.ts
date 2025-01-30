7c173928134f39f99a4454de7fa43380f86ff6fc
Adnan Shihabi <adnan@ashihabi.me>
2025-01-29 17:59:14 -0800

    Authorization: Implement cors and auth0 verification. Update to Fastify 5
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>
    Co-authored-by: Gautam Panakkal <111669555+gpanakkal@users.noreply.github.com>

--- src/envalid.ts
+++ src/envalid.ts
@@ -9,6 +9,11 @@ const cfg = cleanEnv(process.env, {
   MONGO_TESTING_URI: str(),
   SERVICE_PORT: num(),
   SALT_ROUNDS: num(),
+  AUTH0_DOMAIN: str(),
+  AUTH0_AUDIENCE: str(),
+  AUTH0_CLIENT_ID: str(),
+  AUTH0_CLIENT_SECRET: str(),
+  DASHBOARD_URL: str(),
 });
 
 export default cfg;
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import Fastify from 'fastify';
 import mercurius from 'mercurius';
 import mercuriusLogging from 'mercurius-logging';
 import cors from '@fastify/cors';
+import { fastifyAuth0Verify } from 'fastify-auth0-verify';
+import cfg from './envalid.js';
 import { schema } from './graphql/schemas.js';
 import { resolvers } from './graphql/resolvers.js';
-import cfg from './envalid.js';
 import { getClientRoutes } from './routes/client.routes.js';
 
 const server = Fastify({
@@ -16,8 +17,6 @@ const server = Fastify({
 server.get('/healthcheck', async () => ({ status: 'OK' }));
 // register routes for out flag entity
 server.register(getClientRoutes, { prefix: 'api' });
-// todo: replace '*' origin with environment variable referencing dashboard
-server.register(cors, { prefix: 'graphql', origin: '*' });
 server.register(mercurius, {
   schema,
   resolvers,
@@ -25,6 +24,33 @@ server.register(mercurius, {
 });
 
 server.register(mercuriusLogging);
+
+// configure cors
+await server.register(cors, {
+  prefix: 'graphql',
+  origin: cfg.DASHBOARD_URL,
+  credentials: true,
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  maxAge: 86400,
+});
+
+await server.register(fastifyAuth0Verify, {
+  audience: cfg.AUTH0_AUDIENCE,
+  domain: cfg.AUTH0_DOMAIN,
+  secret: cfg.AUTH0_CLIENT_SECRET,
+});
+
+// middleware to authorize all requests
+server.addHook('onRequest', async (request, reply) => {
+  try {
+    console.log(request.headers.authorization);
+    await request.jwtVerify();
+  } catch (error) {
+    console.log(error);
+    reply.code(401).send({ error: 'Unauthorized' });
+  }
+});
 
 server.listen({ port: cfg.SERVICE_PORT }, (error, address) => {
   if (error instanceof Error) {
