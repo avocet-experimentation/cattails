043578440e95e03eaa06c05e3212972827b01612
Gautam Panakkal <gautampanakkal@gmail.com>
2025-02-04 14:54:11 -0800

    Remove SDK-side routes and logic
    Co-authored-by: Adnan Shihabi <adnan@ashihabi.me>
    Co-authored-by: Sean Mentele <56010359+SeanM0124@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ import cors from '@fastify/cors';
 import { schema } from './graphql/schemas.js';
 import { resolvers } from './graphql/resolvers.js';
 import cfg from './envalid.js';
-import { getClientRoutes } from './routes/client.routes.js';
 
 const server = Fastify({
   logger: true,
@@ -14,8 +13,6 @@ const server = Fastify({
 });
 // check if service is up during deployment; check on regular frequency
 server.get('/healthcheck', async () => ({ status: 'OK' }));
-// register routes for out flag entity
-server.register(getClientRoutes, { prefix: 'api' });
 // todo: replace '*' origin with environment variable referencing dashboard
 server.register(cors, { prefix: 'graphql', origin: '*' });
 server.register(mercurius, {
@@ -26,11 +23,11 @@ server.register(mercurius, {
 
 server.register(mercuriusLogging);
 
-server.listen({ port: cfg.SERVICE_PORT }, (error, address) => {
+server.listen({ port: cfg.MANAGEMENT_API_PORT }, (error, address) => {
   if (error instanceof Error) {
     console.error(error);
     process.exit(1);
   }
 
-  console.log(`\ncattails server ready at ${address}`);
+  console.log(`\nAvocet management API running at ${address}`);
 });
